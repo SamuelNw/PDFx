5e27bd208c928eba42ee5d16789569a897378d5b
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-02-24 07:04:15 +0300

    Create the watermark method and initiate the requrements.txt file

--- pdfx.py
+++ pdfx.py
@@ -1,1 +1,52 @@
 # Using PyPDF2 to perform major operations on PDF documents.
+import PyPDF2
+
+
+# Give the paths to said PDF docs.
+pdf_to_watermark = ""
+watermark_pdf = ""
+watermark_output_pdf = ""
+
+
+class PDFx(object):
+    def __init__(self):
+        pass
+
+    def watermark(self, source, watermark, output):
+        """
+        Info: Adds a watermark to all pages of given source.
+        - Initiate a reader object with the source document.
+        - Do the same for the writer object.
+        - Get the number of pages to determine the number of iterations.
+        - Looping through as many times as the number of pages, merge each page
+        with a similar instance of the reader object with the watermark.
+        - Add that result to the writer object earlier created.
+        - After completion of the loops, use the cumulated writer object to write
+        to the output file.
+        """
+        source_reader = PyPDF2.PdfReader(source)
+        num_of_pages = len(source_reader.pages)
+
+        stamp_reader = PyPDF2.PdfReader(watermark)
+
+        writer = PyPDF2.PdfWriter()
+
+        for index in range(num_of_pages):
+            content_page = source_reader.pages[index]
+            # watermark is on the first page
+            watermark_page = stamp_reader.pages[0]
+
+            content_page.merge_page(watermark_page)
+            writer.add_page(content_page)
+
+        with open(output, "wb") as file:
+            writer.write(file)
+
+        print("Done")
+
+    def password_protect(self, *args):
+        pass
+
+
+# pdf_operations = PDFx()
+# pdf_operations.watermark(pdf_to_watermark, watermark_pdf, watermark_output_pdf)
